3bb7574b3351a6f05c095425bc1670a8caa5e36d
Dallas Fraser <dfraser@digitaled.com>
2019-04-29 21:52:18 -0400

    Messenger interface
    This should make it easier to incorporate the bot into other platforms, maybe slack

---
+++ api/messenger/__init__.py
@@ -0,0 +1,35 @@
+'''
+@author: Dallas Fraser
+@author: 2019-08-27
+@organization: MLSB
+@project: Facebook Bot
+@summary: Used so bot can connect to different messengers (Facebook, Kik, etc)
+'''
+
+
+class Messenger():
+    """
+    This is an abstract class that just used to specify the methods
+    that need to be implemented by the messenger
+    """
+
+    def __init__(self):
+        pass
+
+    @abstractmethod
+    def send_message(self, message):
+        """Sends the given message.
+        Parameters:
+            message: the message to send (Message)
+        """
+        raise NotImplementedError("Messenger needs to implement send message")
+
+    @abstractmethod
+    def parse_response(self, response):
+        """Parses the message from the response given by the messenger.
+        Parameters:
+            response: response object (dependent upon messenger)
+        Returns:
+            message: the message parsed from the response (Message)
+        """
+        raise NotImplementedError("Messenger needs to implement parse message")
