c1aa56139b45edc9b0ebffde119482a35689202f
Dallas Fraser <dfraser@digitaled.com>
2019-05-01 21:16:01 -0400

    Todo Test the various players

---
+++ api/test/testPlayers.py
@@ -0,0 +1,33 @@
+'''
+@author: Dallas Fraser
+@author: 2019-04-29
+@organization: MLSB
+@project: Facebook Bot
+@summary: Test captain player
+'''
+from api.players.subscription import Subscription, Subscriptions,\
+    RelativeTimeEnum
+from api.players.captain import Captain
+from api.players.player import Player
+from api.players.convenor import Convenor
+import unittest
+import datetime
+
+
+class TestPlayer(unittest.TestCase):
+
+    def testConversion(self):
+        pass
+
+
+class TestCaptain(unittest.TestCase):
+    pass
+
+
+class TestConvenor(unittest.TestCase):
+    pass
+
+
+if __name__ == "__main__":
+    # import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
