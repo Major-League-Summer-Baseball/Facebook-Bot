99eb47e744d7237ed9f78d65c4b9c3dc4f3be832
Dallas Fraser <dfraser@digitaled.com>
2019-05-08 21:07:47 -0400

    Mongo shouldnt know anything about the messenger

--- api/database/mongo.py
+++ api/database/mongo.py
@@ -5,6 +5,7 @@
 @project: Facebook Bot
 @summary: Holds an implementation of the the database service
 '''
+from api.errors import DatabaseException
 from api.players.player import Player
 
 
@@ -14,9 +15,11 @@ class DatabaseService():
     def __init__(self, mongo):
         self.mongo = mongo
 
-    def get_player(self, facebook_id):
-        """Returns the user object for the given facebook id"""
-        return self.mongo.db.users.find_one({'facebook_id': facebook_id})
+    def get_player(self, messenger_id):
+        """Returns the user object for the given messenger id"""
+        search_parameters = Player.search_parameters(messenger_id)
+        player_dictionary = self.mongo.db.users.find_one(search_parameters)
+        return Player(dictionary=player_dictionary)
 
     def create_player(self, sender_id, name):
         """Creates a user with the given sender_id and given name"""
@@ -24,21 +27,26 @@ class DatabaseService():
         self.mongo.db.users.insert(player)
         return player
 
-    def save_player(self, user):
-        self.mongo.db.users.save(user)
+    def save_player(self, player):
+        if player is None:
+            raise DatabaseException("Saving: a non-existent user (None)")
+        elif not isinstance(player, Player):
+            raise DatabaseException("Saving: player of the wrong type")
+        self.mongo.db.users.save(player)
 
-    def already_in_league(self, player):
+    def already_in_league(self, player_info):
         """Check if a player already in the league
 
         Parameters:
-            player: the player object (MLSB Player)
+            player_info: the player infor from the platform (dictionary)
         Returns:
             taken: True if someone already taken that player, False otherwise
         TODO:
             Currently this method will limit a user to only using one messenger
         """
         taken = True
-        user = self.mongo.db.users.find_one({'player_id': player['player_id']})
+        search_parameters = Player.get_player_search(player_info)
+        user = self.mongo.db.users.find_one(search_parameters)
         if user is None:
             taken = False
         return taken
