eaeb48085e37dd4caed6cfd7503b863e19c0e449
Dallas Fraser <dfraser@digitaled.com>
2019-05-16 19:07:11 -0400

    Small change
    save the action map, Will work on this class next

--- api/actions/welcome.py
+++ api/actions/welcome.py
@@ -2,7 +2,6 @@ from api.actions import ActionInterface
 
 
 class WelcomeAction(ActionInterface):
-    ACTION_IDENTIFIER = "Welcome screen"
 
-    def process(self):
-        pass
+    def process(self, action_map):
+        self.action_map = action_map
