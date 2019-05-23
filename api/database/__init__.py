ac6437e78875c60d9e8165c4fcfad172bc3fb95f
Dallas Fraser <dfraser@digitaled.com>
2019-05-23 17:38:34 -0400

    Database interface
    TODO once the mongo methods are more sorted out

---
+++ api/database/__init__.py
@@ -0,0 +1,11 @@
+'''
+@author: Dallas Fraser
+@author: 2019-04-29
+@organization: MLSB
+@project: Facebook Bot
+@summary: The interface any database service provider needs to implement
+'''
+
+
+class DatabaseInterface():
+    pass
