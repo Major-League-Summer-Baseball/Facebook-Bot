8ed148a7a8a0eec2b554e2c8c02a430f0e62f9b6
Dallas Fraser <dfraser@digitaled.com>
2019-05-22 20:06:54 -0400

    Mongo save - do not duplicate player
    Forgot that mongo use _id to save players, so updated the save function to use that fact. Should be fairly robust now.

--- api/database/mongo.py
+++ api/database/mongo.py
@@ -5,6 +5,7 @@
 @project: Facebook Bot
 @summary: Holds an implementation of the the database service
 '''
+from api.logging import LOGGER
 from api.errors import DatabaseException
 from api.players.player import Player
 
@@ -19,12 +20,14 @@ class DatabaseService():
         """Returns the user object for the given messenger id"""
         search_parameters = Player.get_messenger_search(messenger_id)
         player_dictionary = self.mongo.db.users.find_one(search_parameters)
+        if player_dictionary is None:
+            return None
         return Player(dictionary=player_dictionary)
 
     def create_player(self, sender_id, name):
         """Creates a user with the given sender_id and given name"""
         player = Player(messenger_id=sender_id, name=name)
-        self.mongo.db.users.insert(player)
+        self.mongo.db.users.insert(player.to_dictionary())
         return player
 
     def save_player(self, player):
@@ -32,7 +35,15 @@ class DatabaseService():
             raise DatabaseException("Saving: a non-existent user (None)")
         elif not isinstance(player, Player):
             raise DatabaseException("Saving: player of the wrong type")
-        self.mongo.db.users.save(player)
+        search_parameters = player.messenger_search()
+        player_dictionary = self.mongo.db.users.find_one(search_parameters)
+        if player_dictionary is None:
+            player_dictionary = player.to_dictionary()
+        else:
+            temp = player.to_dictionary()
+            temp["_id"] = player_dictionary["_id"]
+            player_dictionary = temp
+        self.mongo.db.users.save(player_dictionary)
 
     def already_in_league(self, player_info):
         """Check if a player already in the league
