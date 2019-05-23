d737b70fc77284d2cff12f30274ba3596f2d8b4c
Dallas Fraser <dfraser@digitaled.com>
2019-05-23 18:39:48 -0400

    Added home screen to action map

--- api/actions/action_map.py
+++ api/actions/action_map.py
@@ -1,5 +1,8 @@
 from api.actions.identify_user import IdentifyUser
 from api.actions.welcome import WelcomeAction
-from api.settings.action_keys import WELCOME_KEY, IDENTIFY_KEY
+from api.actions.homescreen import HomescreenAction
+from api.settings.action_keys import WELCOME_KEY, IDENTIFY_KEY, HOME_KEY
 
-ACTION_MAP = {WELCOME_KEY: WelcomeAction, IDENTIFY_KEY: IdentifyUser}
+ACTION_MAP = {WELCOME_KEY: WelcomeAction,
+              IDENTIFY_KEY: IdentifyUser,
+              HOME_KEY: HomescreenAction}
