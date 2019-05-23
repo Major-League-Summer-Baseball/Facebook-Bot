ba26e9c484c757825dea65b552925fc48733a5b1
Dallas Fraser <dfraser@digitaled.com>
2019-05-23 18:41:48 -0400

    Added three new strings

--- api/settings/message_strings.py
+++ api/settings/message_strings.py
@@ -12,3 +12,10 @@ LOCKED_OUT = "You have maxed out your tries, try contacting a convenor"
 IMPOSTER = ("Your player account is already used by another user,\n" +
             "contact convenors to resolve")
 WELCOME_LEAGUE = "Welcome to the league: {}"
+ACKNOWLEDGE_CONVENOR = ("Sounds like you are convenor so upgrading you to\n" +
+                        " with this you will be able to submit scores for\n" +
+                        "any team and send league updates to the whole league")
+ACKNOWLEDGE_CAPTAIN = ("Sounds like you the captain of the team\n" +
+                       "you will be able to submit scores for your team")
+PART_OF_TEAM = ("It appears you are on: {}\n" +
+                "you are now subscribed to the team and will get updates")
