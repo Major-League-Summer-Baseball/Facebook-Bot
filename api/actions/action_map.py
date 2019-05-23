a0811e83f1125440a59133ccb7c5ed3848d11cb2
Dallas Fraser <dfraser@digitaled.com>
2019-05-23 17:39:07 -0400

    Using setting action keys

--- api/actions/action_map.py
+++ api/actions/action_map.py
@@ -1,5 +1,5 @@
 from api.actions.identify_user import IdentifyUser
 from api.actions.welcome import WelcomeAction
-from api.variables import WELCOME_KEY, IDENTIFY_KEY
+from api.settings.action_keys import WELCOME_KEY, IDENTIFY_KEY
 
 ACTION_MAP = {WELCOME_KEY: WelcomeAction, IDENTIFY_KEY: IdentifyUser}
