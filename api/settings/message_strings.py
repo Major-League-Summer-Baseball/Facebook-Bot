1e164b0e01a1dbd67c7708547ce7d0876f1003eb
Dallas Fraser <dfraser@digitaled.com>
2019-05-23 17:37:51 -0400

    Holds a list of strings that are used to send messages back

---
+++ api/settings/message_strings.py
@@ -0,0 +1,14 @@
+'''
+@author: Dallas Fraser
+@author: 2019-04-29
+@organization: MLSB
+@project: Facebook Bot
+@summary: Holds the strings used
+'''
+
+ASK_FOR_EMAIL = "What's your email associated with the league"
+EMAIL_NOT_FOUND = "The given email was not found, try another"
+LOCKED_OUT = "You have maxed out your tries, try contacting a convenor"
+IMPOSTER = ("Your player account is already used by another user,\n" +
+            "contact convenors to resolve")
+WELCOME_LEAGUE = "Welcome to the league: {}"
