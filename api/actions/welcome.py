ad7b647a2c0698f6c9100dfc4d73a61179c0023a
Dallas Fraser <dfraser@digitaled.com>
2019-05-23 18:40:40 -0400

    Got welcome action done
    Just need to write a test for it now

--- api/actions/welcome.py
+++ api/actions/welcome.py
@@ -5,8 +5,12 @@
 @project: Facebook Bot
 @summary: welcome new player, registers the players to their teams
 '''
+from api.helper import get_this_year
+from api.message import Message
 from api.logging import LOGGER
 from api.settings.action_keys import HOME_KEY
+from api.settings.message_strings import ACKNOWLEDGE_CONVENOR, PART_OF_TEAM,\
+    ACKNOWLEDGE_CAPTAIN
 from api.actions import ActionInterface, ActionState
 
 
@@ -15,20 +19,45 @@ class WelcomeAction(ActionInterface):
     def process(self, action_map):
         self.action_map = action_map
         messenger_id = self.message.get_sender_id()
+        recipient_id = self.message.get_recipient_id()
         player = self.database.get_player(messenger_id)
 
         # determine if they are a convenor
         if self.is_player_convenor(player):
+            m1 = Message(messenger_id,
+                         message=ACKNOWLEDGE_CONVENOR,
+                         recipient_id=recipient_id)
+            self.messenger.send_message(m1)
             player.make_convenor()
+            teams = self.platform.lookup_all_teams()
+            LOGGER.debug(teams)
+            for team_id in teams.keys():
+                player.add_team({"team_id": team_id})
+                player.make_captain({"team_id": team_id})
         else:
             # add the players to their teams and if they captain make them
             # a captain
-            teams = self.platform.lookup_teams_player_associated_with()
+            teams = self.platform.lookup_teams_player_associated_with(player)
+            LOGGER.debug(teams)
+            current_year = get_this_year()
             for team in teams:
                 # adding the player to the team will subscribe them
-                player.add_team(team)
-                if self.is_player_captain_of_team(player, team):
-                    player.make_captain(team)
+                year = team.get('year', None)
+                if year is not None and year == current_year:
+                    team_name = team.get("team_name", team.get("color",
+                                                               "name unknown"))
+                    m1 = Message(messenger_id,
+                                 message=PART_OF_TEAM.format(team_name),
+                                 recipient_id=recipient_id)
+                    self.messenger.send_message(m1)
+                    player.add_team(team)
+                    if self.is_player_captain_of_team(player, team):
+                        m1 = Message(messenger_id,
+                                     message=ACKNOWLEDGE_CAPTAIN,
+                                     recipient_id=recipient_id)
+                        self.messenger.send_message(m1)
+                        player.make_captain(team)
+        self.successful(player)
 
     def is_player_convenor(self, player):
         """Returns True if the player is a convenor, otherwise False"""
@@ -57,7 +86,7 @@ class WelcomeAction(ActionInterface):
 
     def successful(self, player):
         """The method called when welcome action was successfully completed"""
-        LOGGER.info("Identified player: " + str(player))
+        LOGGER.info("Welcomed player: " + str(player))
 
         # update the player for the next action
         player.set_action_state(ActionState(key=HOME_KEY))
