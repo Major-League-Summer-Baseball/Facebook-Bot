c746159147353f789c9f542103dffd2e597eb56b
Dallas Fraser <dfraser@digitaled.com>
2019-05-29 17:31:32 -0400

    Added a bunch of strings

--- api/settings/message_strings.py
+++ api/settings/message_strings.py
@@ -28,3 +28,43 @@ MAIN_MENU_FUN_TITLE = "Fun Meter"
 MAIN_MENU_SUBMIT_SCORE_TITLE = "Submit Score"
 MAIN_MENU_HR_TITLE = "HR Leaders"
 MAIN_MENU_SS_TITLE = "SS Leaders"
+
+# titles used on homescreen
+EVENTS_TITLE = "Events"
+FUN_TITLE = "Fun Meter"
+HR_TITLE = "HR Leaders:"
+SS_TITLE = "SS Leaders:"
+
+# Commensts used on homescreen
+FUN_TOTAL_COMMENT = "Total fun: {}"
+NOGAMES_COMMENT = "No upcoming games"
+NOT_CAPTAIN_COMMENT = "Appears you are not a captain"
+INTROS = ["What do you want?",
+          "Well, hello there",
+          "DAM Daniel!!, how you doing?",
+          "Next one is on me",
+          "Wet and wild is my name"
+          ]
+SASSY_COMMENT = ["WOW, {} hit hr(s), wind must have been blowing today",
+                 "{} finally bought that corked bat",
+                 "League will be looking into this one: {}",
+                 "Has {} been at the gym lately",
+                 "Let me guess, {} is a lefty on WP2",
+                 "Good for {}"
+                 ]
+COMPLIMENT = ["{} is my favourite player",
+              "Awesome, {} might lead the league one day",
+              "Tell {} to keep it up",
+              "Fuck ya {}",
+              "{} could be in the all-star game"]
+EMOJI = [u'\U0001f37a',
+         u'\U0001f600',
+         u'\U0001f377',
+         u'\U0001f382',
+         u'\u26a0',
+         u'\u26be',
+         u'\U0001f649']
+FUN_COMMENT = ["",
+               "",
+               "What fun",
+               "Are we having too much fun?"]
