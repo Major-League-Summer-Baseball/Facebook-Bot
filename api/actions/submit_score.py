5f71953da6a0597280654f3cadddd820fe0b99e4
Dallas Fraser <dfraser@digitaled.com>
2019-05-30 19:24:56 -0400

    Outline of submit score
    Thinking maybe do it in two parts

---
+++ api/actions/submit_score.py
@@ -0,0 +1,14 @@
+'''
+@author: Dallas Fraser
+@author: 2019-05-23
+@organization: MLSB
+@project: Facebook Bot
+@summary: Action for when a captain/convenor submits a score
+'''
+from api.actions import ActionInterface
+
+
+class HomescreenAction(ActionInterface):
+
+    def process(self, action_map):
+        pass
