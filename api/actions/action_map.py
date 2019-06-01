14965de71e103da0d10ecc7115497796d73a43ce
Dallas Fraser <dfraser@digitaled.com>
2019-05-31 21:38:15 -0400

    Add submit score to action map

--- api/actions/action_map.py
+++ api/actions/action_map.py
@@ -1,8 +1,11 @@
 from api.actions.identify_user import IdentifyUser
 from api.actions.welcome import WelcomeAction
 from api.actions.homescreen import HomescreenAction
-from api.settings.action_keys import WELCOME_KEY, IDENTIFY_KEY, HOME_KEY
+from api.actions.submit_score import SubmitScoreAction
+from api.settings.action_keys import WELCOME_KEY, IDENTIFY_KEY, HOME_KEY,\
+    SUBMIT_SCORE_KEY
 
 ACTION_MAP = {WELCOME_KEY: WelcomeAction,
               IDENTIFY_KEY: IdentifyUser,
-              HOME_KEY: HomescreenAction}
+              HOME_KEY: HomescreenAction,
+              SUBMIT_SCORE_KEY: SubmitScoreAction}
